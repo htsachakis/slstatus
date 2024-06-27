54eb12a64deb33a799bc777bb37f636fad5b1310
Charalampos Tsachakis <htsachakis@gmail.com>
2024-06-27 23:01:21 +0300

    paches

--- config.h
+++ config.h
@@ -65,6 +65,6 @@ static const char unknown_str[] = "n/a";
  */
 static const struct arg args[] = {
 	/* function format          argument */
-	{ datetime, "%s",           "%F %T" },
-	{ keymap,   "%s",	    "c?n>"  },
+	{ datetime, " %s ",           "%F %T" },
+	{ keymap,   " %s ",	    NULL  },
 };
